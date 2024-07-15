b76e2badaa598226b28747c389ae2d747febe3d2
Soltus <liaoshanyi@qq.com>
2024-07-16 00:45:11 +0800

    #134

--- app/src/main/java/sc/windom/sillot/AppActivity.kt
+++ app/src/main/java/sc/windom/sillot/AppActivity.kt
@@ -2,8 +2,8 @@
  * Sillot T☳Converbenk Matrix 汐洛彖夲肜矩阵：为智慧新彖务服务
  * Copyright (c) 2024.
  *
- * lastModified: 2024/7/11 下午10:57
- * updated: 2024/7/11 下午10:57
+ * lastModified: 2024/7/16 上午12:43
+ * updated: 2024/7/16 上午12:43
  */
 
 package sc.windom.sillot
@@ -23,8 +23,31 @@ class AppActivity : ComponentActivity() {
     private val TAG = "AppActivity.kt"
     private val srcPath = thisSourceFilePath(TAG)
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        BuglyLog.d(TAG, "outState: $outState")
+        if (outState.isEmpty) return // avoid crash
+        super.onSaveInstanceState(outState)
+        // 可添加额外需要保存可序列化的数据
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        BuglyLog.d(TAG, "onNewIntent() invoked. @ $intent")
+        init(intent)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        BuglyLog.d(TAG, "onCreate() invoked. @ $intent")
+        init(intent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        BuglyLog.d(TAG, "onDestroy() invoked")
+    }
+
+    private fun init(in2intent: Intent?) {
         var intent = App.currentIntentRef.get()
         if (intent == null) {
             intent = Intent(this, MainActivity::class.java)
@@ -34,13 +57,8 @@ class AppActivity : ComponentActivity() {
                         or Intent.FLAG_ACTIVITY_MULTIPLE_TASK
             )
         }
-        BuglyLog.d(TAG, "intent ${intent}")
+        BuglyLog.d(TAG, "startActivity @ ${intent}")
         startActivity(intent)
         finishAndRemoveTask()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        BuglyLog.d(TAG, "onDestroy() invoked")
     }
 }
